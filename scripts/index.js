69fc1c2f274c62c8d99f48461d9f75c33043f706
Gabriella Franco <gabriella-nena@hotmail.com>
2025-03-12 13:58:30 -0300

    Seção 4 - adicionada responsividade mobile

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,35 @@
+let slideIndex = 1;
+
+function slideResponsivity(position) {
+
+    let slides = document.getElementsByClassName("feature-section-card");
+    let dots = document.getElementsByClassName("dot");
+
+    if (position > dots.length) {
+        slideIndex = 1;
+    } else if (position < 1) {
+        slideIndex = dots.length;
+    } else {
+        slideIndex = position;
+    }
+
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].classList.remove("active");
+    }
+    for (let i = 0; i < dots.length; i++) {
+        dots[i].classList.remove("active");
+    }
+
+    if (slideIndex <= slides.length) {
+        slides[slideIndex - 1].classList.add("active");
+    }
+    dots[slideIndex - 1].classList.add("active");
+}
+
+function currentSlide(i) {
+    slideResponsivity(i);
+}
+
 function toggleMenu() {
     document.querySelector(".navbar ul").classList("show")
 }
